38552c7a7749185a5d0295c5eb2fcda5877248d4
Antoine <antoines@activestate.com>
2024-12-02 11:07:22 -0500

    Tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Expr {
     Unit,
@@ -9,17 +11,25 @@ enum ExprStart {
 
 pub fn parse(str: &str) -> Expr {
     let mut chars = str.chars();
+    let expr = _parse_expr(&mut chars);
+    match chars.next() {
+        Some(c) => panic!("Unexpected character; expected end of input: '{c}'"),
+        None => {}
+    }
+    return expr;
+}
 
+fn _parse_expr(chars: &mut Chars<'_>) -> Expr {
     let start = match chars.next() {
         Some('(') => ExprStart::LParen,
-        Some(c) => panic!("Unexpected character; expected start of expr: {}", c),
+        Some(c) => panic!("Unexpected character; expected start of expr: '{c}'"),
         None => panic!("Unexpected end of input; expected start of expr"),
     };
 
     match start {
         ExprStart::LParen => match chars.next() {
             Some(')') => Expr::Unit,
-            Some(c) => panic!("Unexpected character; expected ')': {}", c),
+            Some(c) => panic!("Unexpected character; expected ')': '{c}'"),
             None => panic!("Unexpected end of input; expected ')'"),
         },
     }
@@ -29,9 +39,41 @@ pub fn parse(str: &str) -> Expr {
 mod tests {
     use super::*;
 
+    // Start
     #[test]
-    fn test_parse_ok() {
+    #[should_panic(expected = "Unexpected end of input; expected start of expr")]
+    fn test_parse_err_start_eof() {
+        parse("");
+    }
+
+    #[test]
+    #[should_panic(expected = "Unexpected character; expected start of expr: '?'")]
+    fn test_parse_err_start_unrecognized() {
+        parse("?");
+    }
+
+    // LParen
+    #[test]
+    #[should_panic(expected = "Unexpected end of input; expected ')'")]
+    fn test_parse_lparen_err_eof() {
+        parse("(");
+    }
+
+    #[test]
+    #[should_panic(expected = "Unexpected character; expected ')': '?'")]
+    fn test_parse_lparen_err_unrecognized() {
+        parse("(?");
+    }
+
+    // Unit
+    #[test]
+    fn test_parse_unit_ok() {
         assert_eq!(parse("()"), Expr::Unit);
-        assert_eq!(parse("() "), Expr::Unit);
+    }
+
+    #[test]
+    #[should_panic(expected = "Unexpected character; expected end of input: '?'")]
+    fn test_parse_unit_err_end_not_eof() {
+        parse("()?");
     }
 }
